d43a672203a13b6f00e750605228587bbbfee6eb
ConnorsC <connorschrisc@gmail.com>
2016-10-11 16:33:39 -0300

    Added a second level and some sounds
    a few changes to mechanics also

---
+++ Assignment1StarterProject/Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelTrigger : MonoBehaviour {
+
+    private GameObject player;
+    public string level = "Level2";
+    public GameObject spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Cooper"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            DontDestroyOnLoad(other.gameObject);
+            SceneManager.LoadScene("Level2");
+            other.transform.position = GameObject.Find("SpawnPoint").transform.position;
+        }
+    }
+}
